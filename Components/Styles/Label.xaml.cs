ece055458aaa84d750bf49fa5a607c8b2509221e
Joshua Sprow <jsprow@gmail.com>
2019-01-14 23:59:08 -0500

    * App.xaml: Multiple Merged ResourceDictionaries... woot. I'm gonna   build React for Xamarin, I think.
    * Label.xaml, Label.xaml.cs, ComponentsPage.xaml:

---
+++ Components/Styles/Label.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using Xamarin.Forms;
+
+namespace Components.Styles
+{
+    public partial class Label : ResourceDictionary
+    {
+        public Label()
+        {
+            InitializeComponent();
+        }
+    }
+}
